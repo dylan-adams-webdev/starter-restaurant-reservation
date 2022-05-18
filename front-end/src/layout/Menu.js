7f17ba708be91115f623a12816c09eb1fb12ccd6
dylan-adams-webdev <dadams31@asu.edu>
2022-05-18 15:41:08 -0700

    create off-canvas menu for small screens (temporary)

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Link } from 'react-router-dom';
 
 /**
@@ -23,17 +21,17 @@ function Menu() {
 				<Link className='navbar-brand' to='/'>
 					Periodic Tables
 				</Link>
-
+				{/* OFFCANVAS START */}
 				<div
 					className='offcanvas offcanvas-start'
-					tabindex='-1'
+					tabIndex='-1'
 					id='offcanvasNavbar'
 					aria-labelledby='offcanvasNavbarLabel'>
 					<div className='offcanvas-header'>
 						<h5
 							className='offcanvas-title'
 							id='offcanvasNavbarLabel'>
-							Offcanvas
+							Periodic Tables
 						</h5>
 						<button
 							type='button'
@@ -43,9 +41,33 @@ function Menu() {
 					</div>
 					<div className='offcanvas-body'>
 						<ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
-							<li className='nav-item'>
-								<Link className='nav-link active' to='/'>
-									Home
+							<li id='dashboard' className='nav-item'>
+								<Link className='nav-link' to='/dashboard'>
+									<span className='oi oi-dashboard'></span>
+									&nbsp;Dashboard
+								</Link>
+							</li>
+							<li id='search' className='nav-item'>
+								<Link className='nav-link' to='/search'>
+									<span className='oi oi-magnifying-glass'></span>
+									&nbsp;Search
+								</Link>
+							</li>
+							<li id='new-reservation' className='nav-item'>
+								<Link
+									className='nav-link'
+									to='/reservations/new'>
+									<span className='oi oi-plus'></span>
+									&nbsp;New Reservation
+								</Link>
+							</li>
+							<li>
+								<Link
+									id='new-table'
+									className='nav-link'
+									to='/tables/new'>
+									<span className='oi oi-layers'></span>
+									&nbsp;New Table
 								</Link>
 							</li>
 						</ul>
@@ -53,53 +75,6 @@ function Menu() {
 				</div>
 			</div>
 		</nav>
-
-		// <nav className="navbar navbar-dark align-items-start p-0">
-		//   <div className="container-fluid d-flex flex-column p-0">
-		//     <Link
-		//       className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
-		//       to="/"
-		//     >
-		//       <div className="sidebar-brand-text mx-3">
-		//         <span>Periodic Tables</span>
-		//       </div>
-		//     </Link>
-		//     <hr className="sidebar-divider my-0" />
-		//     <ul className="nav navbar-nav text-light" id="accordionSidebar">
-		//       <li className="nav-item">
-		//         <Link className="nav-link" to="/dashboard">
-		//           <span className="oi oi-dashboard" />
-		//           &nbsp;Dashboard
-		//         </Link>
-		//       </li>
-		//       <li className="nav-item">
-		//         <Link className="nav-link" to="/search">
-		//           <span className="oi oi-magnifying-glass" />
-		//           &nbsp;Search
-		//         </Link>
-		//       </li>
-		//       <li className="nav-item">
-		//         <Link className="nav-link" to="/reservations/new">
-		//           <span className="oi oi-plus" />
-		//           &nbsp;New Reservation
-		//         </Link>
-		//       </li>
-		//       <li className="nav-item">
-		//         <Link className="nav-link" to="/tables/new">
-		//           <span className="oi oi-layers" />
-		//           &nbsp;New Table
-		//         </Link>
-		//       </li>
-		//     </ul>
-		//     <div className="text-center d-none d-md-inline">
-		//       <button
-		//         className="btn rounded-circle border-0"
-		//         id="sidebarToggle"
-		//         type="button"
-		//       />
-		//     </div>
-		//   </div>
-		// </nav>
 	);
 }
 
