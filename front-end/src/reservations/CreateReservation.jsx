cab830cd840841b23d585b49f69bc8bace52f9fa
dylan-adams-webdev <dadams31@asu.edu>
2022-05-25 14:19:50 -0700

    implement reservation form handler with error state

--- front-end/src/reservations/CreateReservation.jsx
+++ front-end/src/reservations/CreateReservation.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from 'react-router';
 import ReservationForm from './ReservationForm';
+import ErrorAlert from '../error/ErrorAlert';
+import {newReservation} from '../utils/api';
 
 export default function CreateReservation() {
+	const [error, setError] = useState(null);
+	const history = useHistory();
 	const submitHandler = (data) => {
-		console.log(data);
+		const abort = new AbortController();
+		newReservation(data, abort.signal)
+			.then(() => history.push(`/dashboard?date=${data.reservation_date}`))
+			.catch(setError);
+		return () => abort.abort();
 	}
 	
 	return (
 		<>
+			<ErrorAlert error={error} />
 			<ReservationForm submitHandler={submitHandler} />
 		</>
 	);
