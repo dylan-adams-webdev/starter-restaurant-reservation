0d1b3ae35de9dddaf289166e035d2674fa30421b
dylan-adams-webdev <dadams31@asu.edu>
2022-05-25 14:09:35 -0700

    temporarily add logger

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const express = require('express');
+const pinoHttp = require('pino-http');
 const cors = require('cors');
 
 const errorHandler = require('./errors/errorHandler');
@@ -10,11 +11,14 @@ const notFound = require('./errors/notFound');
 const reservationsRouter = require('./reservations/reservations.router');
 const tablesRouter = require('./tables/tables.router');
 
+const logger = pinoHttp({ prettyPrint: true });
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(logger);
 app.use('/reservations', reservationsRouter);
 app.use('/tables', tablesRouter);
 
