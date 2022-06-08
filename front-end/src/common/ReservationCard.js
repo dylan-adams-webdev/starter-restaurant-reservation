63f1817cbdc0af65688ca943ddd8992bb4e66f1d
dylan-adams-webdev <dadams31@asu.edu>
2022-06-08 01:38:12 -0700

    recover from vs code anomaly

--- front-end/src/common/ReservationCard.js
+++ front-end/src/common/ReservationCard.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { DateTime as dt, Duration } from 'luxon';
 import { Link } from 'react-router-dom';
+import { useQueryClient, useMutation } from 'react-query';
+import toast from 'react-hot-toast';
+import { cancelReservation } from '../utils/api';
 
 export default function ReservationCard(props) {
 	const { res } = props;
-	
+
 	const date = dt.fromISO(res.reservation_date);
 	const time = Duration.fromISOTime(res.reservation_time);
 	const dateTime = date.plus(time);
 	
+	const client = useQueryClient();
+	const query = useMutation(cancelReservation, {
+		onSuccess: () => toast.success('Reservation is cancelled'),
+		onError: toast.error,
+		onSettled: () => client.invalidateQueries('reservations')
+	})
 	
+	const cancelHandler = () => {
+		const confirm = window.confirm('Do you want to cancel this reseration?\nThis cannot be undone.');
+		if (confirm) query.mutate({reservation_id: res.reservation_id});
+	}
 
 	return (
 		<div className='col-md-4 m-2'>
@@ -42,18 +55,35 @@ export default function ReservationCard(props) {
 								&nbsp;{res.people}
 							</div>
 						</div>
+					</div>
+					<div className='d-flex justify-content-between mt-2'>
+						<div>
+							<Link
+								to={`/reservations/${res.reservation_id}/edit`}
+								href={`/reservations/${res.reservation_id}/edit`}
+								className='btn btn-outline-secondary me-2'
+							>
+								Edit
+							</Link>
+							<button
+								type='button'
+								data-reservation-id-cancel={res.reservation_id}
+								onClick={cancelHandler}
+								className='btn btn-outline-danger'
+							>
+								Cancel
+							</button>
+						</div>
 						<div>
 							{res.status === 'booked' ? (
 								<Link
-								className='btn btn-outline-primary'
-								href={`/reservations/${res.reservation_id}/seat`}
-								to={`reservations/${res.reservation_id}/seat`}
-							>
-								Seat
-							</Link>
-							) : (
-									null
-							)}
+									className='btn btn-outline-primary'
+									href={`/reservations/${res.reservation_id}/seat`}
+									to={`reservations/${res.reservation_id}/seat`}
+								>
+									Seat
+								</Link>
+							) : null}
 						</div>
 					</div>
 				</div>
